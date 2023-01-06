a598326dd3a3c7ec0e72b3c7cad7ebf7bbba2afa
Renat <104959479+RenatKhrashchevskyi@users.noreply.github.com>
2023-01-06 15:44:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var min = function(list){
+  let minV = Math.min(...list)
+      return minV;
+  }
+  
+  
+  var max = function(list){
+      let maxV = Math.max(...list);
+      return maxV;
+  }
